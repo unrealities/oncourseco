34f384a009dcb8417796d9f03653ed602dc74756
Tom Szymanski <szymanski.tom@gmail.com>
2015-08-10 16:15:46 -0600

    Set Creds

--- oncourse.go
+++ oncourse.go
@@ -3,6 +3,9 @@ package main
 import (
 	"net/http"
 
+	"google.golang.org/appengine"
+	"google.golang.org/appengine/datastore"
+
 	"github.com/julienschmidt/httprouter"
 )
 
@@ -19,6 +22,30 @@ func routes() http.Handler {
 	router := httprouter.New()
 
 	router.GET("/data", Data)
+	router.GET("/SetOAuthCredentials", SetOAuthCredentials)
 
 	return router
+}
+
+// Only to be used manually to update OAuth Credentials.
+// Never store any keys, tokens or secrets in the code.
+func SetOAuthCredentials(w http.ResponseWriter, r *http.Request, _ httprouter.Params) {
+	c := appengine.NewContext(r)
+
+	t := &Credentials{
+		ClientId:     "",
+		ClientSecret: ""}
+
+	k := datastore.NewKey(c, "Credentials", "OAuth", 0, nil)
+
+	_, err := datastore.Put(c, k, t)
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
+}
+
+type Credentials struct {
+	ClientId     string
+	ClientSecret string
 }
