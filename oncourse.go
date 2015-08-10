e79ec065d54095ad449db93f2969283b9621e319
Tom Szymanski <szymanski.tom@gmail.com>
2015-08-10 13:11:35 -0600

    deeper

--- oncourse.go
+++ oncourse.go
@@ -3,10 +3,22 @@ package main
 import (
 	"net/http"
 
-	"github.com/unrealities/oncourseco/routers"
+	"github.com/julienschmidt/httprouter"
 )
 
 func init() {
-	r := routers.Routes()
+	r := routes()
 	http.Handle("/", r)
+}
+
+func data(w http.ResponseWriter, r *http.Request, _ httprouter.Params) {
+	w.Write([]byte("winning"))
+}
+
+func routes() http.Handler {
+	router := httprouter.New()
+
+	router.GET("/data", data)
+
+	return router
 }
